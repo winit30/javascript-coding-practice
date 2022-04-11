29229ea0c87e5e13edb1496a8174812b505b112f
Vineet Mishra <vntmshra2@gmail.com>
2022-04-11 14:57:03 +0530

    AddNumbers Added

---
+++ AddNumbers.js
@@ -0,0 +1,43 @@
+function addNumbers(a, b) {
+
+    let string1
+    let string2
+
+    if(a.length === b.length) {
+      string1 = a;
+      string2 = b;
+    } else {
+       string1 = a.length > b.length ? a : b;
+       string2 = a.length < b.length ? a : b;
+    }
+
+    const diff = 1 + string1.length - string2.length;
+
+    const numberArray = [];
+
+    let temp = 0;
+
+    for(let i = string1.length; i > 0; i--) {
+
+        let secondVal = 0;
+
+        if(parseInt(string2[i-diff])) {
+            secondVal = parseInt(string2[i-diff]);
+        }
+
+         const add = parseInt(string1[i - 1]) + secondVal + temp;
+
+         temp = 0;
+
+         if(i == 1) {
+           numberArray.push(add)
+         } else if(add < 10) {
+             numberArray.push(add)
+         } else {
+           temp = 1
+           numberArray.push(add - 10)
+         }
+    }
+
+    return numberArray.reverse().join('');
+}

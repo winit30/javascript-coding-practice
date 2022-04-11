36ed6d3ce1d07ed5907944b1f0f179f2968132e8
Vineet Mishra <vntmshra2@gmail.com>
2022-04-11 20:40:10 +0530

    Update arr

--- arraySubset.js
+++ arraySubset.js
@@ -10,3 +10,34 @@ function arraySubset(arr, sub) {
     }
     return true;
 }
+
+function arraySubset2(arr, sub) {
+    if(sub.length > arr.length) {
+        return false;
+    }
+  
+    const arrCount = {};
+  
+    for(let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if(arrCount[item] !== undefined) {
+            arrCount[item]++;
+        } else {
+            arrCount[item] = 1;
+        }
+    }
+    
+    for(let i = 0; i < sub.length; i++) {
+        const currentItem = sub[i];
+        if(arrCount[currentItem] === undefined) {
+            return false;
+        }
+        
+        arrCount[currentItem]--;
+        if(arrCount[currentItem] === 0) {
+            delete arrCount[currentItem];
+        }
+    }
+    
+    return true;
+}

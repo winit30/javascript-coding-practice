8fc98c58721a571055296135d38d4abd54b9ddbc
Vineet Mishra <vntmshra2@gmail.com>
2022-04-20 01:37:46 +0530

    Update quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,5 +1,27 @@
 const arr = [5, 7, 1, 2, 8, 6, 4, 3];
 
+//Just an other way to partition
+function partitionTwo(nums, low, high) {
+
+  const pivot = nums[high]
+
+  let j = low;
+
+  for(let i = low; i < high; i++) {
+      if(nums[i] < pivot) {
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        j++;
+      }
+  }
+
+  nums[high] = nums[j]
+  nums[j] = pivot;
+  return j;
+
+}
+
 function partition(nums, low, high) {
 
   const pivot = nums[low];
@@ -25,19 +47,14 @@ function partition(nums, low, high) {
 
   nums[low] = nums[j];
   nums[j] = pivot;
-
   return j;
 
 }
 
 let quickSortRec = function(nums, low, high) {
-
   if (high > low) {
-
     let pivotIndex = partition(nums, low, high);
-
     quickSortRec(nums, low, pivotIndex - 1);
-
     quickSortRec(nums, pivotIndex + 1, high);
   }
 };
